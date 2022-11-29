46644cf4d6faecad3b8a5595640eef0bba7b9782
WithWings889 <ironwoman889@gmail.com>
2022-11-29 20:58:08 +0200

    add all currencies list

---
+++ src/app/ui/currencies-calculator/currencies-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-currencies-calculator',
+  templateUrl: './currencies-calculator.component.html',
+  styleUrls: ['./currencies-calculator.component.scss']
+})
+export class CurrenciesCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
